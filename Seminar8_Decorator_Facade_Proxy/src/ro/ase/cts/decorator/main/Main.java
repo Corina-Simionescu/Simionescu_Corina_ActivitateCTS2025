54a86a44912e928ad90be610e1fc18568469534b
Corina717 <corina.simionescu12@gmail.com>
2025-05-18 11:01:41 +0300

    Add solved problem name

--- Seminar8_Decorator_Facade_Proxy/src/ro/ase/cts/decorator/main/Main.java
+++ Seminar8_Decorator_Facade_Proxy/src/ro/ase/cts/decorator/main/Main.java
@@ -4,6 +4,8 @@ import ro.ase.cts.decorator.clase.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Solved problem: Problems CTS2025 - Restaurant -> A6\\n");
+
         Nota nota =  new NotaDePlata(100);
 
         NotaDecorata notaDecorata = new NotaRevelion(nota);
